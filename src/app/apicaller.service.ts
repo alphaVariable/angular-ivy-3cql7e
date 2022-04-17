3c3354e71b5d83b6e1e6a459ef220a20e3faac04
alphaVariable <vikasoffice81@gmail.com>
2022-04-17 14:26:19 +0800

    1 file modified

--- src/app/apicaller.service.ts
+++ src/app/apicaller.service.ts
@@ -2,11 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 // import rxjs map
 import { map } from 'rxjs/operators';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApicallerService {
+  private _myreply = new BehaviorSubject<any[]>(null);
+  cast = this._myreply.asObservable();
   constructor(private _http: HttpClient) {}
 
   // get data from swapi.dev
@@ -17,5 +20,9 @@ export class ApicallerService {
     return this._http
       .get('https://swapi.dev/api/people/')
       .pipe(map((result) => result['results']));
+  }
+
+  editRes(nextRes) {
+    this._myreply.next(nextRes);
   }
 }
