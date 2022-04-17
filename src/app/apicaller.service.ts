07d5cb7e3a5978cd21fcc95761db3b60cbe82273
Vikas Thakur <vikasoffice81@gmail.com>
2022-04-17 12:30:14 +0800

    adds httpclient

--- src/app/apicaller.service.ts
+++ src/app/apicaller.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallerService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  // get data from swapi.dev
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,7 +9,7 @@ import { Component1Component } from './component1/component1.component';
 import { Component2Component } from './component2/component2.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule],
   declarations: [ AppComponent, HelloComponent, Component1Component, Component2Component ],
   bootstrap:    [ AppComponent ]
 })
