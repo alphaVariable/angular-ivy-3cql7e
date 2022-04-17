2c70060ba01c46c9385378aa2a8564f5e97f9238
alphaVariable <vikasoffice81@gmail.com>
2022-04-17 13:01:45 +0800

    1 file modified

--- src/app/component1/component1.component.ts
+++ src/app/component1/component1.component.ts
@@ -4,22 +4,16 @@ import { ApicallerService } from '../apicaller.service';
 @Component({
   selector: 'app-component1',
   templateUrl: './component1.component.html',
-  styleUrls: ['./component1.component.css']
+  styleUrls: ['./component1.component.css'],
 })
 export class Component1Component implements OnInit {
-
   apiresponse: any = {};
-  constructor(
-    private _apicaller: ApicallerService
-  ) { }
+  constructor(private _apicaller: ApicallerService) {}
 
   ngOnInit(): void {
     // get data from api using service
-    this._apicaller.getData().subscribe(
-      (data) => {
-        this.apiresponse = data;
-      }
-    );
+    this._apicaller.getData().subscribe((data: any[]) => {
+      this.apiresponse = data;
+    });
   }
-
 }
