bdd09c720f7b256b71ef24a9d6d6537dbf21edef
alphaVariable <vikasoffice81@gmail.com>
2022-04-17 13:22:19 +0800

    1 file modified

--- src/app/apicaller.service.ts
+++ src/app/apicaller.service.ts
@@ -1,22 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-// import rxjs map
-import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApicallerService {
-
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {}
 
   // get data from swapi.dev
   getData() {
-    console.log('RequestID:'+Math.random() * 100 + ' getData()');
+    console.log('RequestID:' + Math.random() * 100 + ' getData()');
     // count api request
     // return as array of objects
-    return this._http.get('https://swapi.dev/api/people/').pipe(
-      map(result => result['results'])
-    );
+    return this._http
+      .get('https://swapi.dev/api/people/')
+      .pipe(map((result) => result['results']));
   }
 }
