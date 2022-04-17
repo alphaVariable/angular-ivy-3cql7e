0a61392d2e06e05d728b977656d2fc36f3090567
alphaVariable <vikasoffice81@gmail.com>
2022-04-17 13:22:25 +0800

    1 file modified

--- src/app/apicaller.service.ts
+++ src/app/apicaller.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// import rxjs map
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
